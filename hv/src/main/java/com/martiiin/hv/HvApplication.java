a045bc04b5ab8580121f882330b27d7470239380
MarTiiin <30706039@qq.com>
2019-09-06 17:50:22 +0800

    添加页面 基本流播放功能

--- hv/src/main/java/com/martiiin/hv/HvApplication.java
+++ hv/src/main/java/com/martiiin/hv/HvApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.io.File;
 
@@ -23,9 +26,16 @@ public class HvApplication {
 
 
     @Bean
-    public File[] getDiskList(){
-        return File.listRoots();
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(HvApplication.class, args);
--- hv/src/main/java/com/martiiin/hv/controller/HVController.java
+++ hv/src/main/java/com/martiiin/hv/controller/HVController.java
@@ -3,39 +3,76 @@ package com.martiiin.hv.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.martiiin.hv.config.FileJSON;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-@RestController
+@Controller
 public class HVController {
 
 
     @Autowired
     FileJSON fileJSON;
 
-
-    @RequestMapping("getVideo")
-    public String disk(){
-        System.out.println(fileJSON.getJson());
-
+    @ResponseBody
+    @RequestMapping("video/play/{id}")
+    public void play(HttpServletResponse response,@PathVariable("id") String id) throws Exception {
+        fileJSON.read();
         JSONObject json = fileJSON.getJson();
-
-        if (json!=null){
-            return json.toJSONString();
-        }else {
-            return "nothing";
-        }
+        Map map = json.getInnerMap();
+        String filePath = (String) map.get(id);
+        File file = new File(filePath);
 
 
+        InputStream in = new FileInputStream(file);
 
+        response.addHeader("Content-Type","video/mp4;charset=utf-8");
+        IOUtils.copy(in,response.getOutputStream());
+        response.flushBuffer();
 
     }
 
+
+
+    @RequestMapping("index")
+    public String index(Model model) {
+        fileJSON.read();
+        JSONObject json = fileJSON.getJson();
+        Map map = json.getInnerMap();
+        List list = new ArrayList();
+        System.out.println(map.keySet());
+        map.forEach((k,v)->{
+            list.add(k);
+        });
+        model.addAttribute("list",list);
+        return "video/index.html";
+    }
+
+
+    @RequestMapping("video/{id}")
+    public String video(Model model, @PathVariable("id") String id, HttpServletResponse response) throws Exception {
+
+
+
+        model.addAttribute("name",id);
+        return "video/VideoDemo.html";
+    }
+
+
+
+    @ResponseBody
     @RequestMapping("scanAll")
     public List getVideoFile(){
         JSONObject jsonObject = new JSONObject();
